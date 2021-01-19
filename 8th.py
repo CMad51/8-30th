30f6b130a4e9b9f56824a8bd45561674d9382485
CMad51 <77188621+CMad51@users.noreply.github.com>
2021-01-19 08:25:33 +0330

    Add files via upload

---
+++ 8th.py
@@ -0,0 +1,16 @@
+student={'name':'mamaD',
+                 'last name' :'mamuD',
+                 'skills':['swimming','geek'],
+                 'marital status': True,
+                 'age':30,
+                 'his family':{'father':'abuD',
+                                    'wife':'hanuD',
+                                     'sis':'ganuD' 
+                                    }
+                 }
+
+#print(student.get('skills'))
+
+student.pop('age')
+
+print (student)
